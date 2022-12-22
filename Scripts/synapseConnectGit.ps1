6a955a085ceb2e6bcb51d66eeda494328a34b6d6
JunchiLiu94 <juncliu@microsoft.com>
2022-12-22 17:48:10 +1100

    v3

--- Scripts/synapseConnectGit.ps1
+++ Scripts/synapseConnectGit.ps1
@@ -1,10 +1,14 @@
 param(
+    [string]$subscriptionID,
     [string]$repositoryType,
     [string]$accountName,
     [string]$repositoryName,
     [string]$collaborationBranch,
     [string]$synapseWorkspaceName
 )
+
+#Set Azure Subscription
+Set-AzContext -Subscription $subscriptionID
 
 #Configures Git Repo for Synapse Workspace
 $config = New-AzSynapseGitRepositoryConfig -RepositoryType $repositoryType -AccountName $accountName -RepositoryName $repositoryName -CollaborationBranch $collaborationBranch
