623fd93c72b37d88860deb18b286b7780dd4c848
JunchiLiu94 <juncliu@microsoft.com>
2022-12-23 12:11:25 +1100

    v3

--- Scripts/synapseConnectGit.ps1
+++ Scripts/synapseConnectGit.ps1
@@ -8,13 +8,8 @@ param(
 )
 
 #Configures Git Repo for Synapse Workspace
-Select-AzSubscription -SubscriptionId $subscriptionId
+Set-AzContext -Subscription $subscriptionId
 Write-Host $subscriptionId
-Write-Host $repositoryType
-Write-Host $accountName
-Write-Host $repositoryName
-Write-Host $collaborationBranch
-Write-Host $synapseWorkspaceName
 
 try {
     $config = New-AzSynapseGitRepositoryConfig -RepositoryType $repositoryType -AccountName $accountName -RepositoryName $repositoryName -CollaborationBranch $collaborationBranch
